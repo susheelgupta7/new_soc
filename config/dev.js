d82102dc19c576ca15c3483a6c0ac0cff48c768a
susheelgupta7 <38013283+susheelgupta7@users.noreply.github.com>
2021-08-10 23:02:39 +0530

    Delete config directory

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleClientID: '107101271539-pv6tssefdueq32t8aureb32t5v3c1oc8.apps.googleusercontent.com',
-    googleClientSecret: '7TYUGIZP9QrO9fqf7wIVgjA-',
-    mongoURI: 'mongodb+srv://susheel-7:Asdf@123@cluster0.qkm3z.mongodb.net/FriendsCircle?retryWrites=true&w=majority',
-    cookieKey: 'iahjkgthkloiuyhbvfredswdcftgdesrvbh',
-    baseURL: ''
-};
