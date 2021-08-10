d07450dc5b8c140575a1d01d5588189632fc02c2
susheelgupta7 <38013283+susheelgupta7@users.noreply.github.com>
2021-08-10 23:03:12 +0530

    Delete models directory

--- models/Jobs.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const jobSchema = new Schema({
-    jobId: String,
-    companyName: String,
-});
-
-mongoose.model('jobs', jobSchema);
