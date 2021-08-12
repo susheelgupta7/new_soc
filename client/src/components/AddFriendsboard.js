0de6fbbc66b8a5929d2dfeb35b3bc74fedb719f4
susheelgupta7 <38013283+susheelgupta7@users.noreply.github.com>
2021-08-12 11:23:27 +0530

    Update AddFriendsboard.js

--- client/src/components/AddFriendsboard.js
+++ client/src/components/AddFriendsboard.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+class AddFriend extends Component {
+    constructor(props) {
+        super();
+        this.state ={
+            users:[]
+        }
+    }
 
-
-
-class feedboard extends Component {
     async componentDidMount() {
-        
-        const comp = await axios.get(`${process.env.PUBLIC_URL}/api/get_all_users`);
+        const userArr = await axios.get(`${process.env.PUBLIC_URL}/api/get_all_user`); //api check
         this.setState({
-            listofusers: comp.data
-        });
-        this.fetchUsers();
-    }   
-    
+            users: userArr.data
+        })
+    }
     render() {
-        return (
-            <div> </div>
+        const userList = this.state.user;
+        return(
+            <div className="Adduser">
+                {
+                    userList.map(user=>(
+                        <div className="user">
+                            <p>{user.user}</p>
+                        </div>
+                    ))
+                }
+            </div>
         )
     }
 }
-export default feedboard;
-
-
+export default AddFriend;
