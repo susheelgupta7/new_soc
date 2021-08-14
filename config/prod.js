27a1440d3d0f5b0a87d026699d5bbd207f02ed52
susheelgupta7 <38013283+susheelgupta7@users.noreply.github.com>
2021-08-14 09:42:11 +0530

    Delete config directory

--- config/prod.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-module.exports = {
-    googleClientID: process.env.googleClientID,
-    googleClientSecret: process.env.googleClientSecret,
-    mongoURI: process.env.mongoURI,
-    cookieKey: process.env.cookieKey,
-    baseURL: process.env.baseURL
-};
